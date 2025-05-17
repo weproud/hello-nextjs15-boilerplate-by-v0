e85d56d496a1647f86b0d31a269fdb8ddc6acf4d
betheproud <betheproud@gmail.com>
2025-05-17 18:11:22 +0900

    feat: pretendard font 적용

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # hello-nextjs15-boilerplate-by-v0
 
-## authjs
+## Project Summary
 
-## shadcn/ui
+## Project Architecture
 
-## next-themes
+## Tech Stack
+
+### authjs
+
+### shadcn/ui
+
+### next-themes
 
 https://www.sujalvanjare.com/blog/dark-mode-nextjs15-tailwind-v4
+
+## Etc
+
+### Pretendard font
+
+https://white-blank.tistory.com/213
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ThemeProvider from "@/components/theme-provider";
 import { Providers } from "./providers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { pretendard } from "@/lib/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${pretendard.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
