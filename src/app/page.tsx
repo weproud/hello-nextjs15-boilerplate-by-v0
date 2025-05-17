5225a958db57f1cf3f24ae0bbd6757c9f2577b3d
betheproud <betheproud@gmail.com>
2025-05-17 17:23:37 +0900

    feat: shadcn/ui 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        main
+        <Label className="text-4xl font-black">
+          Hello Nextjs 15 Boilerplate by v0
+        </Label>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        footer
-      </footer>
     </div>
   );
 }
